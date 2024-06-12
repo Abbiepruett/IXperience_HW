5842787be530b606e601c01f0421803ae740cab2
Abbiepruett <abbiepruett@yahoo.com>
2024-06-12 17:04:04 +0200

    Created leetCode solutions folder

---
+++ Solutions/twoSum.py
@@ -0,0 +1,34 @@
+class Solution(object):
+
+    nums = [2,7,11,15]
+    target = 9
+
+    def twoSum( nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        complementMap = dict()
+
+        for i in range(len(nums)):
+            num = nums[i]
+            complement = target - num
+            
+            if num in complementMap:
+                return [complementMap[num], i]
+            else:
+                complementMap[complement] = i
+
+
+    answer = twoSum(nums, target)
+    print("The answer is: " + str(answer))
+
+
+    #brute force 
+    # double for loop, check if each pairing = target
+
+    
+
+        
